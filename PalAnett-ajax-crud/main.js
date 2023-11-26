1a0e292acdd15874eb6dbc07dc00d1abd198d993
xnetta92 <palanett92@gmail.com>
2023-11-26 13:37:59 +0100

    javitas

--- PalAnett-ajax-crud/main.js
+++ PalAnett-ajax-crud/main.js
@@ -4,15 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const employeeData = document.getElementById("employeeData");
   fetch(api_url).then(httpResponse => httpResponse.json())
   .then(responseBody => {
-    responseBody.forEach(person => {
+    responseBody.forEach(employees => {
       const tableRow =`<tr>
-      <td>${person.id}</td>
-      <td>${person.name}</td>
-      <td>${person.email}</td>
-      <td>${person.title}</td>
-      <td>${person.address}</td>
-      <td>${person.salary}</td>
-      <td>${person.birthdate}</td>
+      <td>${employees.id}</td>
+      <td>${employees.name}</td>
+      <td>${employees.email}</td>
+      <td>${employees.title}</td>
+      <td>${employees.address}</td>
+      <td>${employees.salary}</td>
+      <td>${employees.birthdate}</td>
       </tr>`;
     })
   }); 
