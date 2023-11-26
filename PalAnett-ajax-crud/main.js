9ad47b0ccf7b2f8db516bb5292f2c2235b9087fe
xnetta92 <palanett92@gmail.com>
2023-11-26 13:36:21 +0100

    tableformat

--- PalAnett-ajax-crud/main.js
+++ PalAnett-ajax-crud/main.js
@@ -2,5 +2,18 @@ const api_url = "https://retoolapi.dev/Nag12s/employees"
 
 document.addEventListener("DOMContentLoaded", () => {
   const employeeData = document.getElementById("employeeData");
-  fetch(api_url).then(httpResponse => console.log(httpResponse));
+  fetch(api_url).then(httpResponse => httpResponse.json())
+  .then(responseBody => {
+    responseBody.forEach(person => {
+      const tableRow =`<tr>
+      <td>${person.id}</td>
+      <td>${person.name}</td>
+      <td>${person.email}</td>
+      <td>${person.title}</td>
+      <td>${person.address}</td>
+      <td>${person.salary}</td>
+      <td>${person.birthdate}</td>
+      </tr>`;
+    })
+  }); 
 })
