4e2e4e5fc80a3e6b4fcab519c772ab6dd51e5612
xnetta92 <palanett92@gmail.com>
2023-11-26 14:43:13 +0100

    javitsokadik

--- PalAnett-ajax-crud/main.js
+++ PalAnett-ajax-crud/main.js
@@ -4,16 +4,31 @@ document.addEventListener("DOMContentLoaded", () => {
   const employeeData = document.getElementById("employeeData");
   fetch(api_url).then(httpResponse => httpResponse.json())
   .then(responseBody => {
+    employeeData.innerHTML = "";
     responseBody.forEach(person => {
-      const tableRow =`<tr>
-      <td>${person.id}</td>
-      <td>${person.name}</td>
-      <td>${person.email}</td>
-      <td>${person.title}</td>
-      <td>${person.address}</td>
-      <td>${person.salalry}</td>
-      <td>${person.birthdate}</td>
-      </tr>`;
+      const tableRow = document.createElement("tr");
+      const idTableData = document.createElement("td");
+      const nameTableData = document.createElement("td");
+      const emailTableData = document.createElement("td");
+      const titleTableData = document.createElement("td");
+      const addressTableData = document.createElement("td");
+      const salalryTableData = document.createElement("td");
+      const birthdateTableData = document.createElement("td");
+      idTableData.textContent = person.id;
+      nameTableData.textContent = person.name;
+      emailTableData.textContent = person.email;
+      titleTableData.textContent = person.title;
+      addressTableData.textContent = person.address;
+      salalryTableData.textContent = person.salalry;
+      birthdateTableData.textContent = person.birthdate;
+      tableRow.appendChild(idTableData);
+      tableRow.appendChild(nameTableData);
+      tableRow.appendChild(emailTableData);
+      tableRow.appendChild(titleTableData);
+      tableRow.appendChild(addressTableData);
+      tableRow.appendChild(salalryTableData);
+      tableRow.appendChild(birthdateTableData);
+      employeeData.appendChild(tableRow);
     })
   }); 
 })
